a3ad48e42f6552f5fc9cfb189b59ee231ac29ab5
Kensandoval-Ing <ken.sandoval@duocuc.cl>
2026-05-18 20:55:23 +0000

    fix: Añadido --network host al docker run para permitir salida a internet

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ echo "Construyendo imagen Docker..."
 docker build --network host -t app-covid .
 
 echo "Ejecutando contenedor..."
-docker run --name samplerunning --env API_URL="https://disease.sh/v3/covid-19" app-covid
+docker run --network host --name samplerunning --env API_URL="https://disease.sh/v3/covid-19" app-covid
