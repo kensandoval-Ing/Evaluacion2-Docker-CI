eb85cfaeee45cfc84868436cfcc2922bda63482a
Kensandoval-Ing <ken.sandoval@duocuc.cl>
2026-05-18 20:51:50 +0000

    fix: Añadido --network host al build para solucionar problema de DNS de la VM

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-
-
-
-
+#!/bin/bash
 echo "Construyendo imagen Docker..."
-docker build -t app-covid .
+docker build --network host -t app-covid .
+
 echo "Ejecutando contenedor..."
 docker run --name samplerunning --env API_URL="https://disease.sh/v3/covid-19" app-covid
